f6f1f4512ff464f5d40ada768ec4cd7db1d399ad
Norman <rickey00x@gmail.com>
2021-06-23 20:25:30 +0200

    feat:added php wrapper to get url parameters

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+# Load XML and stylesheet
+$xmldoc = new DOMDocument();
+$xsldoc = new DOMDocument();
+$xsl = new XSLTProcessor();
+
+$xmldoc->loadXML(file_get_contents('dashboard.xml'));
+$xsldoc->loadXML(file_get_contents('xsl/dashboard.xsl'));
+
+# Error handling on load
+libxml_use_internal_errors(true);
+$result = $xsl->importStyleSheet($xsldoc);
+if (!$result) {
+    foreach (libxml_get_errors() as $error) {
+        echo "Libxml error: {$error->message}\n";
+    }
+}
+libxml_use_internal_errors(false);
+
+# Parameters
+$startDate = isset($_GET['startDate']) ? $_GET['startDate'] : date('Ymd');
+$endDate = isset($_GET['endDate']) ? $_GET['endDate'] : date('Ymd');
+
+$xsl->setParameter('', 'timeframestart', $startDate);
+$xsl->setParameter('', 'timeframeend', $endDate);
+
+# Transform
+if ($result) {
+    echo $xsl->transformToXML($xmldoc);
+}
